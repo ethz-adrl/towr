a2653f16bc01a8b63774c7817d18516d0cbae81b
Alexander Winkler <winklera@ethz.ch>
2016-04-03 13:58:46 +0200

    cleaned up "ConstructSplineSequence"

--- src/spline_container.cc
+++ src/spline_container.cc
@@ -39,6 +39,18 @@ double SplineContainer::GetTotalTime(bool exclude_4ls_splines) const
 }
 
 
+int SplineContainer::GetSplineID(double t_global) const
+{
+   int t = 0;
+   for (ZmpSpline s: splines_) {
+   t += s.duration_;
+
+   if (t >= t_global)
+     return s.id_;
+   }
+}
+
+
 void SplineContainer::AddSpline(const ZmpSpline &spline)
 {
   splines_.push_back(spline);
@@ -57,35 +69,26 @@ void SplineContainer::ConstructSplineSequence(
   int step = 0;
   unsigned int id = 0; // unique identifiers for each spline
 
-  const int kSplinesPer4ls = 1;
-  const int kSplinesPerStep = 1;
+  // first 4ls-phase and step
+  AddSpline(ZmpSpline(id++, t_stance_initial, true, step));
+  AddSpline(ZmpSpline(id++, t_swing, false, step));
 
-  for (size_t i = 0; i < step_sequence.size(); ++i)
+  for (size_t i = 1; i < step_sequence.size(); ++i)
   {
-    // 1. insert 4ls-phase when switching between disjoint support triangles
-    if (i==0) {
-      AddSpline(ZmpSpline(id++, t_stance_initial, true, step));
-    } else {
-      xpp::hyq::LegID swing_leg = step_sequence[i];
-      xpp::hyq::LegID swing_leg_prev = step_sequence[i-1];
-      if (xpp::hyq::SupportPolygonContainer::Insert4LSPhase(swing_leg_prev, swing_leg))
-        for (int s = 0; s < kSplinesPer4ls; s++)
-          AddSpline(ZmpSpline(id++, t_stance/kSplinesPer4ls, true, step));
-    }
+    step++;
 
+    // 1. insert 4ls-phase when switching between disjoint support triangles
+    xpp::hyq::LegID swing_leg = step_sequence[i];
+    xpp::hyq::LegID swing_leg_prev = step_sequence[i-1];
+    if (xpp::hyq::SupportPolygonContainer::Insert4LSPhase(swing_leg_prev, swing_leg))
+      AddSpline(ZmpSpline(id++, t_stance, true, step));
 
     // insert swing phase splines
-    for (int s = 0; s < kSplinesPerStep; s++)
-      AddSpline(ZmpSpline(id++, t_swing/kSplinesPerStep, false, step));
-
-
-
-    // always have last 4ls spline for robot to move into center of feet
-    if (i==step_sequence.size()-1)
-      AddSpline(ZmpSpline(id++, t_stance_final, true, step));
-
-    step++;
+    AddSpline(ZmpSpline(id++, t_swing, false, step));
   }
+
+  // always have last 4ls spline for robot to move into center of feet
+  AddSpline(ZmpSpline(id++, t_stance_final, true, step));
 }
 
 
