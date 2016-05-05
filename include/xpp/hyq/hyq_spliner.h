44c6f8a14f1e2756ede1413fe5dd36cfbf5f5493
Alexander Winkler <winklera@ethz.ch>
2016-05-05 10:50:52 +0200

    added hyq node creation in hyq_spliner

--- include/xpp/hyq/hyq_spliner.h
+++ include/xpp/hyq/hyq_spliner.h
@@ -11,12 +11,32 @@
 #include "hyq_state.h"
 #include <xpp/utils/orientation.h>
 #include <xpp/utils/spliner_3d.h>
+#include <xpp/zmp/zmp_spline.h>
 
 #include <log4cxx/logger.h>
 #include <Eigen/Dense>
 
 namespace xpp {
 namespace hyq {
+
+
+struct SplineNode {
+  typedef ::xpp::utils::QuinticSpliner Spliner;
+  typedef ::xpp::utils::Spliner3d< Spliner > Spliner3d;
+  typedef Spliner3d::Point Point;
+
+
+  Point pos;
+  Point ori;
+  LegDataMap<Point> feet;
+  int swingleg;             // leg to reach this state
+  double T;                 // time to reach this state
+  SplineNode(const Point& _pos, const Point& _ori,
+             const LegDataMap<Point>& _feet, int _swingleg, double _T)
+      : pos(_pos), ori(_ori), feet(_feet), swingleg(_swingleg), T(_T) {};
+};
+
+
 
 /**
 @brief Splines the base pose (position + orientation).
@@ -27,17 +47,33 @@ frame values omega (rollPitchYawToEar)
  */
 class HyqSpliner {
 public:
+  typedef xpp::zmp::ZmpSpline ZmpSpline;
+  typedef std::vector<ZmpSpline> VecZmpSpline;
+  typedef Eigen::Vector3d Vector3d;
+  typedef Foothold::VecFoothold VecFoothold;
 
 public:
   HyqSpliner();
   virtual ~HyqSpliner() {};
+
+
+  static std::vector<SplineNode> BuildStepSequence(const HyqState& P_init,
+                                            const VecZmpSpline& zmp_splines,
+                                            const VecFoothold& footholds,
+                                            double robot_height);
 
   /**
   @brief transforms a HyqState into a collection of Points, including
          body position, body orientation, and feet position
   @param[in] time_to_reach how long the robot has to achieve this state
    */
-  void AddNode(const HyqState& state, double t_max);
+  static SplineNode BuildNode(const HyqState& state, double t_max);
+
+  void AddNode(const HyqState& state, double t_max)
+  {
+    nodes_.push_back(BuildNode(state, t_max));
+  }
+
 
 
   void SetCurrGoal(uint des_goal);
@@ -56,23 +92,15 @@ public:
   void ClearNodes() { nodes_.clear(); }
 
 
+
+  // fixme, make private
+  std::vector<SplineNode> nodes_; // the discrete states to spline through
+
 private:
   typedef ::xpp::utils::QuinticSpliner Spliner;
   typedef ::xpp::utils::Spliner3d< Spliner > Spliner3d;
   typedef Spliner3d::Point Point;
 
-  struct SplineNode {
-    Point pos;
-    Point ori;
-    LegDataMap<Point> feet;
-    int swingleg;             // leg to reach this state
-    double T;                 // time to reach this state
-    SplineNode(const Point& _pos, const Point& _ori,
-               const LegDataMap<Point>& _feet, int _swingleg, double _T)
-        : pos(_pos), ori(_ori), feet(_feet), swingleg(_swingleg), T(_T) {};
-  };
-
-  std::vector<SplineNode> nodes_; // the discrete states to spline through
   uint curr_goal_;                 // current goal node
 
   double kUpswingPercent;       // how long to swing up during swing
--- src/hyq_spliner.cc
+++ src/hyq_spliner.cc
@@ -33,7 +33,80 @@ void HyqSpliner::SetParams(double upswing,
 }
 
 
-void HyqSpliner::AddNode(const HyqState& state, double t_max)
+std::vector<SplineNode>
+HyqSpliner::BuildStepSequence(const HyqState& P_init,
+                              const VecZmpSpline& zmp_splines,
+                              const VecFoothold& footholds,
+                              double robot_height)
+{
+  std::vector<SplineNode> nodes;
+  using namespace xpp::utils::coords_wrapper; // pulls in X, Y, Z..
+
+
+  // start node
+  nodes.push_back(BuildNode(P_init, std::numeric_limits<double>::infinity()));
+
+  /** Add state sequence based on footsteps **/
+  // desired state after first 4 leg support phase
+  HyqState P_plan_prev = P_init;
+  P_plan_prev.ZeroVelAcc();
+  for (hyq::LegID l : hyq::LegIDArray) {
+    P_plan_prev.feet_[l].p(Z) = 0.0;
+  }
+  P_plan_prev.base_.pos.p(Z) = robot_height + P_plan_prev.GetZAvg(); // height of footholds
+
+  for (const ZmpSpline& s : zmp_splines)
+  {
+    // copy a few values from previous state
+    HyqState P_plan = P_plan_prev;
+    P_plan.swingleg_ = false;
+
+    // only change state in swingphase
+    if (!s.IsFourLegSupport()) {
+      const Foothold& f = footholds.at(s.GetCurrStep());
+      P_plan.swingleg_[f.leg] = true;
+      P_plan.feet_[f.leg].p(X) = f.p(X);
+      P_plan.feet_[f.leg].p(Y) = f.p(Y);
+      P_plan.feet_[f.leg].p(Z) = f.p(Z);
+
+      // adjust orientation depending on footholds
+      std::array<Vector3d, kNumSides> avg = P_plan.GetAvgSides();
+      // fixme calculate distance based on current foothold position, not fixed values
+      double width_hip = 0.414;
+      double length_hip = 0.747;
+      double i_roll  = std::atan2((avg[LEFT_SIDE](Z) - avg[RIGHT_SIDE](Z)), width_hip);
+      double i_pitch = std::atan2((avg[HIND_SIDE](Z) - avg[FRONT_SIDE](Z)), length_hip);
+
+
+      // how strictly the body should follow difference in foothold height
+      double roll_gain = 0.0;
+      double pitch_gain = 0.0;
+      // Quaternion is the same for angle += i*pi
+      kindr::rotations::eigen_impl::EulerAnglesXyzPD yprIB(
+          roll_gain*i_roll,
+          pitch_gain*i_pitch,
+          0.0); // P_yaw_des.at(s.step_));
+
+
+      kindr::rotations::eigen_impl::RotationQuaternionPD qIB(yprIB);
+      P_plan.base_.ori.q = qIB.toImplementation();
+
+      // adjust global z position of body depending on footholds
+      P_plan.base_.pos.p(Z) = robot_height + P_plan.GetZAvg(); // height of footholds
+    }
+
+
+    nodes.push_back(BuildNode(P_plan, s.GetDuration()));
+    P_plan_prev = P_plan;
+  }
+
+
+  return nodes;
+}
+
+
+SplineNode
+HyqSpliner::BuildNode(const HyqState& state, double t_max)
 {
   kindr::rotations::eigen_impl::RotationQuaternionPD qIB(state.base_.ori.q);
 
@@ -58,7 +131,8 @@ void HyqSpliner::AddNode(const HyqState& state, double t_max)
   yprIB_full.setYaw(rpyIB.yaw() + counter360*2*M_PI);
   Point ori = Point(yprIB_full.toImplementation());
 
-  nodes_.push_back(SplineNode(state.base_.pos, ori, state.feet_, state.SwinglegID(), t_max));
+
+  return SplineNode(state.base_.pos, ori, state.feet_, state.SwinglegID(), t_max);
 }
 
 
