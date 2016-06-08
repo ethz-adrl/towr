050988c251f314ff6c13bd1fc766a27292a95839
Alexander Winkler <winklera@ethz.ch>
2016-06-08 11:50:03 +0200

    added interpreting_observer class to add context to optimization variables

---
+++ include/xpp/zmp/interpreting_observer.h
@@ -0,0 +1,50 @@
+/**
+ @file    interpreting_observer.h
+ @author  Alexander W. Winkler (winklera@ethz.ch)
+ @date    Jun 8, 2016
+ @brief   Brief description
+ */
+
+#ifndef USER_TASK_DEPENDS_XPP_OPT_INCLUDE_XPP_ZMP_INTERPRETING_OBSERVER_H_
+#define USER_TASK_DEPENDS_XPP_OPT_INCLUDE_XPP_ZMP_INTERPRETING_OBSERVER_H_
+
+#include <xpp/zmp/i_observer.h>
+#include <xpp/zmp/optimization_variables_interpreter.h>
+
+#include <memory> // std::shared_ptr
+
+namespace xpp {
+namespace zmp {
+
+/** @brief Puts the optimization variables into context
+  *
+  * This class is responsible for observing the current values of the
+  * optimization variables and interpreting them based on the problem structure.
+  */
+class InterpretingObserver : public IObserver {
+public:
+  typedef std::vector<xpp::hyq::Foothold> VecFoothold;
+  typedef std::vector<ZmpSpline> VecSpline;
+  typedef Eigen::VectorXd SplineCoefficients;
+  typedef xpp::utils::StdVecEigen2d Footholds;
+  typedef std::shared_ptr<OptimizationVariablesInterpreter> InterpreterPtr;
+
+  InterpretingObserver (OptimizationVariables& subject);
+  virtual ~InterpretingObserver ();
+
+  void SetInterpreter(const InterpreterPtr&);
+
+  void Update() override;
+  VecSpline GetSplines() const;
+  VecFoothold GetFootholds() const;
+
+private:
+  InterpreterPtr interpreter_;
+  SplineCoefficients x_coeff_;
+  Footholds x_feet_;
+};
+
+} /* namespace zmp */
+} /* namespace xpp */
+
+#endif /* USER_TASK_DEPENDS_XPP_OPT_INCLUDE_XPP_ZMP_INTERPRETING_OBSERVER_H_ */
---
+++ src/interpreting_observer.cc
@@ -0,0 +1,51 @@
+/**
+ @file    interpreting_observer.cc
+ @author  Alexander W. Winkler (winklera@ethz.ch)
+ @date    Jun 8, 2016
+ @brief   Brief description
+ */
+
+#include <xpp/zmp/interpreting_observer.h>
+#include <xpp/zmp/optimization_variables.h>
+
+namespace xpp {
+namespace zmp {
+
+InterpretingObserver::InterpretingObserver (OptimizationVariables& subject)
+    :IObserver(subject)
+{
+  interpreter_ = nullptr;
+}
+
+InterpretingObserver::~InterpretingObserver ()
+{
+  // TODO Auto-generated destructor stub
+}
+
+void
+InterpretingObserver::SetInterpreter (const InterpreterPtr& interpreter)
+{
+  interpreter_ = interpreter; // now also interpreter_ has ownership of Interpreter-object owned by interpreter
+}
+
+void
+InterpretingObserver::Update ()
+{
+  x_coeff_ = subject_->GetSplineCoefficients();
+  x_feet_  = subject_->GetFootholdsStd();
+}
+
+InterpretingObserver::VecSpline
+InterpretingObserver::GetSplines () const
+{
+  return interpreter_->GetSplines(x_coeff_);
+}
+
+InterpretingObserver::VecFoothold
+InterpretingObserver::GetFootholds () const
+{
+  return interpreter_->GetFootholds(x_feet_);
+}
+
+} /* namespace zmp */
+} /* namespace xpp */
