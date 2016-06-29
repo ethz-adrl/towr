022c4eccb8814d2d52af37c823d8da7acd91fff1
Alexander Winkler <winklera@ethz.ch>
2016-06-29 12:14:17 +0200

    added robot interface

---
+++ include/xpp/hyq/robot_interface.h
@@ -0,0 +1,65 @@
+/**
+ @file    robot_interface.h
+ @author  Alexander W. Winkler (winklera@ethz.ch)
+ @date    Jun 27, 2016
+ @brief   Brief description
+ */
+
+#ifndef USER_TASK_DEPENDS_XPP_OPT_INCLUDE_XPP_HYQ_ROBOT_INTERFACE_H_
+#define USER_TASK_DEPENDS_XPP_OPT_INCLUDE_XPP_HYQ_ROBOT_INTERFACE_H_
+
+#include <xpp/utils/geometric_structs.h>
+#include <xpp/hyq/leg_data_map.h>
+
+#include <Eigen/Dense>
+
+namespace xpp {
+namespace hyq {
+
+/** Abstracts the robot/simulation and allows to interact with it.
+  *
+  * This class is responsible for providing the current state of the
+  * real robot/simulation and passing commands of the controller to the robot.
+  */
+class RobotInterface {
+public:
+  typedef Eigen::VectorXd JointState;
+  typedef JointState Torques;
+  typedef Eigen::Matrix<double, 6, 1> SpatialAcceleration;
+  typedef Eigen::Vector3d FootXYZ;
+  typedef xpp::utils::Pose Pose;
+
+  RobotInterface ();
+  virtual ~RobotInterface ();
+
+  virtual void StartStateEstimation() const = 0;
+
+  virtual JointState GetJointPosition() const = 0;
+  virtual JointState GetJointVelocity() const = 0;
+  virtual Pose GetBodyPose() const = 0;
+
+
+  virtual double GetControlLoopInterval() const = 0;
+
+  virtual void SetDesiredJointPosition(const JointState& q_des) const = 0;
+  virtual void SetDesiredJointVelocity(const JointState& qd_des) const = 0;
+  virtual void SetDesiredTorque(const Torques& uff) const = 0;
+
+  virtual void StopRobot() const = 0;
+  virtual Torques CalcRequiredTorques(const JointState& q_des,
+                                          const JointState& qd_des,
+                                          const JointState& qdd_des,
+                                          const SpatialAcceleration& i_base_acc_des,
+                                          const LegDataMap<bool>& swinglegs) const = 0;
+
+  virtual LegDataMap<FootXYZ> GetFeetPositions() const = 0;
+  virtual JointState EstimateDesiredJointVelocity(const JointState& q_des,
+                                                  bool use_q_as_prev) const = 0;
+  virtual JointState EstimateDesiredJointAcceleration(const JointState& qd_des,
+                                                    bool use_curr_as_prev) const = 0;
+};
+
+} /* namespace hyq */
+} /* namespace xpp */
+
+#endif /* USER_TASK_DEPENDS_XPP_OPT_INCLUDE_XPP_HYQ_ROBOT_INTERFACE_H_ */
---
+++ src/robot_interface.cc
@@ -0,0 +1,25 @@
+/**
+ @file    robot_interface.cc
+ @author  Alexander W. Winkler (winklera@ethz.ch)
+ @date    Jun 27, 2016
+ @brief   Brief description
+ */
+
+#include <xpp/hyq/robot_interface.h>
+
+namespace xpp {
+namespace hyq {
+
+RobotInterface::RobotInterface ()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+RobotInterface::~RobotInterface ()
+{
+  // TODO Auto-generated destructor stub
+}
+
+} /* namespace hyq */
+} /* namespace xpp */
