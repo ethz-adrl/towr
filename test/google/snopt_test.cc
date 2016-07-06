2fb534d430eb49c71a14c40cff88be8f58602128
Alexander Winkler <winklera@ethz.ch>
2016-07-06 17:40:17 +0200

    Included cleaned-up snoptA test

--- test/google/snopt_test.cc
+++ test/google/snopt_test.cc
@@ -4,21 +4,12 @@
 #include <string.h>
 #include <iostream>
 
-extern "C" {
-void toyusrf_ ( int    *Status, int *n,    double x[],
+extern "C" void toyusrf_ ( int    *Status, int *n,    double x[],
                 int    *needF,  int *neF,  double F[],
                 int    *needG,  int *neG,  double G[],
                 char   *cu,     int *lencu,
                 int    iu[],    int *leniu,
                 double ru[],    int *lenru );
-
-void toyusrfg_( int    *Status, int *n,    double x[],
-                int    *needF,  int *neF,  double F[],
-                int    *needG,  int *neG,  double G[],
-                char   *cu,     int *lencu,
-                int    iu[],    int *leniu,
-                double ru[],    int *lenru);
-}
 
 // sntoya.cpp
 #include "snoptProblem.hpp"
@@ -43,58 +34,12 @@ void toyusrf_(int    *Status, int *n,    double x[],
   //                x0 >= 0.
   //
   //==================================================================
+  // Remember: Cannot include constant terms in here if the constraints
+  // are linear in the variables. Snopt will just ignore them. The
+  // constants MUST be put into the bounds if the constraint is linear.
   F[0] =  x[1];
   F[1] =  x[0]*x[0] + 4*x[1]*x[1];
   F[2] = (x[0] - 2)*(x[0] - 2) + x[1]*x[1];
-}
-
-void toyusrfg_( int    *Status, int *n,    double x[],
-		int    *needF,  int *neF,  double F[],
-		int    *needG,  int *neG,  double G[],
-		char      *cu,  int *lencu,
-		int    iu[],    int *leniu,
-		double ru[],    int *lenru )
-{
-  //==================================================================
-  // Computes the nonlinear objective and constraint terms for the toy
-  // problem featured in the SnoptA users guide.
-  // neF = 3, n = 2.
-  //
-  //   Minimize     x1
-  //
-  //   subject to   x0^2       + 4*x1^2  <= 4,
-  //               (x0 - 2)^2  +   x1^2  <= 5,
-  //                x0 >= 0.
-  //
-  // The triples (g(k),iGfun(k),jGvar(k)), k = 1:neG, define
-  // the sparsity pattern and values of the nonlinear elements
-  // of the Jacobian.
-  //==================================================================
-
-  if ( *needF > 0 ) {
-    F[0] =  x[1]; //  Objective row
-    F[1] =  x[0]*x[0] + 4*x[1]*x[1];
-    F[2] = (x[0] - 2)*(x[0] - 2) + x[1]*x[1];
-  }
-
-
-  if ( *needG > 0 ) {
-    // iGfun[0] = 1
-    // jGvar[0] = 0
-    G[0] = 2*x[0];
-
-    // iGfun[1] = 1
-    // jGvar[1] = 1
-    G[1] = 8*x[1];
-
-    // iGfun[2] = 2
-    // jGvar[2] = 0
-    G[2] = 2*(x[0] - 2);
-
-    // iGfun[3] = 2
-    // jGvar[3] = 1
-    G[3] = 2*x[1];
-  }
 }
 
 TEST(SnoptTest, sntoyA)
@@ -156,75 +101,9 @@ TEST(SnoptTest, sntoyA)
   ToyProb.setIntParameter( "Derivative option", 0 );
   ToyProb.setIntParameter( "Verify level ", 3 );
 
-
   // Solve the problem.
   // snJac is called implicitly in this case to compute the Jacobian.
   ToyProb.solve( Cold );
-
-
-  printf("\nSolving toy1 problem using derivatives...\n");
-
-  // Reset the variables and solve ...
-
-  int lenA   = 6;
-  int *iAfun = new int[lenA];
-  int *jAvar = new int[lenA];
-  double *A  = new double[lenA];
-
-  int lenG   = 6;
-  int *iGfun = new int[lenG];
-  int *jGvar = new int[lenG];
-
-  int neA, neG; // neA and neG must be defined when providing dervatives
-
-  xstate[0] =   0;  xstate[1] = 0;
-  Fmul[0]   =   0;  Fmul[0]   = 0; Fmul[0] =    0;
-  x[0]      = 1.0;
-  x[1]      = 1.0;
-
-
-  // Provide the elements of the Jacobian explicitly.
-  iGfun[0] = 1;
-  jGvar[0] = 0;
-
-  iGfun[1] = 1;
-  jGvar[1] = 1;
-
-  iGfun[2] = 2;
-  jGvar[2] = 0;
-
-  iGfun[3] = 2;
-  jGvar[3] = 1;
-  neG      = 4;
-
-  iAfun[0] = 0;
-  jAvar[0] = 1;
-  A[0]     = 1.0;
-  neA      = 1;
-
-  ToyProb.setProbName    ( "Toy1" );         // Give the problem a new name for Snopt.
-
-  // neA and neG must be set here
-  ToyProb.setA           ( lenA, neA, iAfun, jAvar, A );
-  ToyProb.setG           ( lenG, neG, iGfun, jGvar );
-
-  ToyProb.setUserFun     ( toyusrfg_ );      // Sets the usrfun that supplies G and F.
-
-  ToyProb.setPrintFile   ( "Toy1.out" );
-  ToyProb.setSpecsFile   ( "sntoya.spc" );
-  ToyProb.setIntParameter( "Derivative option", 1 );
-  ToyProb.setIntParameter( "Major Iteration limit", 250 );
-  ToyProb.setIntParameter( "Verify level ", 3 );
-  ToyProb.solve          ( Cold );
-
-  for (int i = 0; i < n; i++ ){
-    cout << "x = " << x[i] << " xstate = " << xstate[i] << endl;
-  }
-  for (int i = 0; i < neF; i++ ){
-    cout << "F = " << F[i] << " Fstate = " << Fstate[i] << endl;
-  }
-
-  ToyProb.solve          ( Warm );
 
   for (int i = 0; i < n; i++ ){
     cout << "x = " << x[i] << " xstate = " << xstate[i] << endl;
@@ -236,14 +115,9 @@ TEST(SnoptTest, sntoyA)
   EXPECT_NEAR(0,x[0], 1e-5);
   EXPECT_NEAR(-1,x[1],1e-5);
 
-
-  delete []iAfun;  delete []jAvar;  delete []A;
-  delete []iGfun;  delete []jGvar;
-
   delete []x;      delete []xlow;   delete []xupp;
   delete []xmul;   delete []xstate;
 
   delete []F;      delete []Flow;   delete []Fupp;
   delete []Fmul;   delete []Fstate;
-
 }
