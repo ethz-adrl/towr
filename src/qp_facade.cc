43d6c33ee6953a37a75c1aafd7342d7661ac7b85
Alexander Winkler <winklera@ethz.ch>
2016-08-25 14:13:00 +0200

    removed dependency on ros of qp facade

--- src/qp_facade.cc
+++ src/qp_facade.cc
@@ -15,8 +15,7 @@
 #include <xpp/utils/eigen_quadprog-inl.h>
 
 #include <cmath>      // std::numeric_limits
-// refactor don't have ros dependency here, this should only be in optimized node
-#include <ros/console.h>
+#include <ctime>      // std::clock()
 
 namespace xpp {
 namespace zmp {
@@ -57,13 +56,11 @@ QpFacade::SolveQp(const State& initial_state,
   xpp::hyq::SupportPolygonContainer supp_polygon_container;
   supp_polygon_container.Init(start_stance, steps, hyq::SupportPolygon::GetDefaultMargins());
 
-  ROS_INFO_STREAM("Start polygon:\n" << supp_polygon_container.GetStartPolygon());
+//  std::cout << "Start polygon:\n" << supp_polygon_container.GetStartPolygon());
   std::vector<hyq::SupportPolygon> supp = supp_polygon_container.GetSupportPolygons();
 
-  ROS_INFO_STREAM("Support polygons for the " << supp.size() << " steps:\n");
-  for (const hyq::SupportPolygon& s : supp) {
-    ROS_INFO_STREAM(s);
-  }
+  std::cout << "number of support polygons: " << supp.size() << std::endl;
+//  for (const hyq::SupportPolygon& s : supp) std::cout << s;
 
   ZmpConstraintBuilder zmp_constraint(spline_structure, robot_height);
   MatVecVec zmp_constr = zmp_constraint.CalcZmpConstraints(supp_polygon_container);
@@ -73,8 +70,8 @@ QpFacade::SolveQp(const State& initial_state,
 //  std::cout << "zmp_constr.Mv.v" << zmp_constr.Mv.v.transpose() << std::endl;
 //  std::cout << "zmp_constr.constant" << zmp_constr.constant.transpose() << std::endl;
 
-  ROS_INFO_STREAM("Initial state:\t" << initial_state);
-  ROS_INFO_STREAM("Final state:\t" << final_state);
+  std::cout << "Initial state:\t" << initial_state << std::endl;
+  std::cout << "Final state:\t" << final_state << std::endl;
 
   Eigen::VectorXd opt_abcd = EigenSolveQuadprog();
   spline_structure->SetCoefficients(opt_abcd);
@@ -87,11 +84,11 @@ QpFacade::EigenSolveQuadprog()
 {
   Eigen::VectorXd opt_spline_coeff_xy;
 
-  ROS_INFO_STREAM("n = " << cost_function_.M.rows() << " variables");
-  ROS_INFO_STREAM("m_eq = " << equality_constraints_.M.rows() << " equality constraints");
-  ROS_INFO_STREAM("m_in = " << inequality_constraints_.M.rows() << " inequality constraints");
+  std::cout << "n = " << cost_function_.M.rows() << " variables\n";
+  std::cout << "m_eq = " << equality_constraints_.M.rows() << " equality constraints\n";
+  std::cout << "m_in = " << inequality_constraints_.M.rows() << " inequality constraints\n";
 
-  ROS_INFO("QP optimizer running...");
+  std::cout << "QP optimizer running...\n";
 
   clock_t start = std::clock();
 
@@ -107,7 +104,7 @@ QpFacade::EigenSolveQuadprog()
   if (cost == std::numeric_limits<double>::infinity())
     throw std::length_error("Eigen::quadprog did not find a solution");
   else
-    ROS_INFO_STREAM("QP optimizer solved in " << time << " ms.");
+    std::cout << "QP optimizer solved in " << time << " ms.\n";
 
   std::cout << "cost: " << cost << std::endl;
   std::cout << "x: \n" << opt_spline_coeff_xy.transpose() << std::endl;
