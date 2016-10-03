a3a1af2cb54f4b8691cf6be0205ea8b23575f921
Alexander Winkler <winklera@ethz.ch>
2016-10-03 10:40:55 +0200

    split 5th order polynomial from ComPolynomial

--- include/xpp/zmp/com_polynomial.h
+++ include/xpp/zmp/com_polynomial.h
@@ -8,74 +8,16 @@
 #ifndef _XPP_ZMP_COM_POLYNOMIAL_H_
 #define _XPP_ZMP_COM_POLYNOMIAL_H_
 
+#include <xpp/utils/polynomial_fifth_order.h>
 #include <xpp/utils/base_state.h>
-
-namespace xpp {
-namespace zmp {
-
-static const int kCoeffCount = 6;
-enum SplineCoeff { A=0, B, C, D, E, F };
-
-struct CoeffValues {
-  double x[kCoeffCount];
-  double y[kCoeffCount];
-  CoeffValues()
-  {
-    for (int c = A; c <= F; ++c)
-      x[c] = y[c] = 0.0;
-  };
-
-  CoeffValues(double xa, double xb, double xc, double xd, double xe, double xf,
-              double ya, double yb, double yc, double yd, double ye, double yf)
-  {
-    x[A] = xa; x[B] = xb; x[C] = xc; x[D] = xd; x[E] = xe; x[F] = xf;
-    y[A] = ya; y[B] = yb; y[C] = yc; y[D] = yd; y[E] = ye; y[F] = yf;
-  }
-
-  /** generates random spline coefficients between -25 and 25 */
-  void SetRandom()
-  {
-    for (int c = A; c <= F; ++c) {
-      x[c] = (double)rand() / RAND_MAX * 50 - 25;
-      y[c] = (double)rand() / RAND_MAX * 50 - 25;
-    }
-  }
-};
-
-
-/** Builds a 5th order polynomial from the polynomial coefficients.
-  */
-class PolynomialFifthOrder {
-public:
-  typedef Eigen::Vector2d Vec2d;
-  typedef xpp::utils::MotionDerivative PosVelAcc;
-
-  PolynomialFifthOrder();
-  PolynomialFifthOrder(const CoeffValues &coeff_values);
-  virtual ~PolynomialFifthOrder() {};
-
-  Vec2d GetState(PosVelAcc whichDeriv, double t) const;
-  void SetSplineCoefficients(const CoeffValues &coeff_values = CoeffValues());
-  double GetCoefficient(int dim, SplineCoeff coeff) const;
-
-private:
-  static constexpr int kDim2d = xpp::utils::kDim2d;
-
-protected:
-  double spline_coeff_[kDim2d][kCoeffCount];
-  friend class SplineContainerTest_EandFCoefficientTest_Test;
-
-};
-
-} // namespace zmp
-} // namespace xpp
 
 namespace cmo {namespace ros{ class RosHelpers; }};
 
 namespace xpp {
 namespace zmp {
 
-/** A fifth order spline that now holds some context information.
+/** A fifth order spline that now holds some context information about the
+  *  Center of Mass (CoM).
   */
 class ComPolynomial : public PolynomialFifthOrder {
 public:
