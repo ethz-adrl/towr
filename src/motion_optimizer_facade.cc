cdfe2456ce4077c99a742fa007050e4557f195c0
Alexander Winkler <winklera@ethz.ch>
2017-03-16 16:51:35 +0100

    ready to delete all the old crap

--- src/motion_optimizer_facade.cc
+++ src/motion_optimizer_facade.cc
@@ -7,8 +7,9 @@
 
 #include <xpp/optimization_variables.h>
 #include <xpp/opt/motion_optimizer_facade.h>
-#include <xpp/opt/motion_structure.h>
-#include <xpp/opt/wb_traj_generator.h>
+#include <xpp/opt/com_motion.h>
+//#include <xpp/opt/motion_structure.h>
+//#include <xpp/opt/wb_traj_generator.h>
 
 #include <xpp/opt/endeffectors_motion.h>
 
@@ -29,30 +30,30 @@ MotionOptimizerFacade::~MotionOptimizerFacade ()
 void
 MotionOptimizerFacade::OptimizeMotion (NlpSolver solver)
 {
-  int curr_phase = start_geom_.GetCurrentPhase();
-  int phases_in_cycle = motion_type_->GetOneCycle().size();
-  int phase = curr_phase%phases_in_cycle;
-
-  MotionParameters::SwinglegPhaseVec phase_specs;
-  phase_specs.push_back({{}, 0.4}); // initial 4-leg support phase
-  for (int i = 0; i<motion_type_->opt_horizon_in_phases_; ++i) {
-
-    phase_specs.push_back(motion_type_->GetOneCycle().at(phase));
-
-    phase++;
-    if (phase == phases_in_cycle)
-      phase = 0;
-  }
-  phase_specs.push_back({{}, 0.8}); // empty vector = final four leg support phase also for contacts at end
-
-
-  MotionStructure motion_structure;
-  motion_structure.Init(motion_type_->robot_ee_,
-                        start_geom_.GetEEPos(),
-                        phase_specs,
-                        start_geom_.GetPercentPhase(),
-                        motion_type_->dt_nodes_ );
-  motion_phases_ = motion_structure.GetPhases();
+//  int curr_phase = start_geom_.GetCurrentPhase();
+//  int phases_in_cycle = motion_type_->GetOneCycle().size();
+//  int phase = curr_phase%phases_in_cycle;
+//
+//  MotionParameters::SwinglegPhaseVec phase_specs;
+//  phase_specs.push_back({{}, 0.4}); // initial 4-leg support phase
+//  for (int i = 0; i<motion_type_->opt_horizon_in_phases_; ++i) {
+//
+//    phase_specs.push_back(motion_type_->GetOneCycle().at(phase));
+//
+//    phase++;
+//    if (phase == phases_in_cycle)
+//      phase = 0;
+//  }
+//  phase_specs.push_back({{}, 0.8}); // empty vector = final four leg support phase also for contacts at end
+//
+//
+//  MotionStructure motion_structure;
+//  motion_structure.Init(motion_type_->robot_ee_,
+//                        start_geom_.GetEEPos(),
+//                        phase_specs,
+//                        start_geom_.GetPercentPhase(),
+//                        motion_type_->dt_nodes_ );
+//  motion_phases_ = motion_structure.GetPhases();
 
   auto goal_com = goal_geom_;
   goal_com.p += motion_type_->offset_geom_to_com_;
@@ -125,7 +126,7 @@ MotionOptimizerFacade::BuildOptimizationStartState (const RobotStateCartesian& c
     feet_zero_z_W.At(ee).p.z() = 0.0;
 
   start_geom_ = curr;
-  start_geom_.SetPercentPhase(0.0);
+//  start_geom_.SetPercentPhase(0.0);
   start_geom_.SetEEState(feet_zero_z_W);
 }
 
