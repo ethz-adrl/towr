dd4d323e9def85745352f93e25473cb2552dc4c8
Alexander Winkler <winklera@ethz.ch>
2017-09-07 18:21:46 +0200

    first simulation experiments working with anymal

--- src/nlp_optimizer_node.cc
+++ src/nlp_optimizer_node.cc
@@ -21,7 +21,7 @@
 #include <rosconsole/macros_generated.h>
 #include <std_msgs/Int32.h>
 
-//#include <kindr/rotations/Rotation.hpp>
+#include <kindr/rotations/Rotation.hpp>
 
 #include <xpp/optimization_parameters.h>
 #include <xpp/state.h>
@@ -43,8 +43,7 @@ NlpOptimizerNode::NlpOptimizerNode ()
 
   current_state_sub_ = n.subscribe(xpp_msgs::robot_state_current,
                                    1, // take only the most recent information
-                                   &NlpOptimizerNode::CurrentStateCallback, this,
-                                   ::ros::TransportHints().tcpNoDelay());
+                                   &NlpOptimizerNode::CurrentStateCallback, this);
   ROS_INFO_STREAM("Subscribed to " << current_state_sub_.getTopic());
 
   cart_trajectory_pub_  = n.advertise<xpp_msgs::RobotStateCartesianTrajectory>
@@ -61,17 +60,39 @@ void
 NlpOptimizerNode::CurrentStateCallback (const xpp_msgs::RobotStateCartesian& msg)
 {
   auto curr_state = RosConversions::RosToXpp(msg);
-  motion_optimizer_.initial_ee_W_ = curr_state.GetEEPos();
+//  motion_optimizer_.initial_ee_W_ = curr_state.GetEEPos();
 
   motion_optimizer_.inital_base_     = State3dEuler(); // zero
   motion_optimizer_.inital_base_.lin = curr_state.base_.lin;
+//  motion_optimizer_.inital_base_.lin.v_.setZero();
+//  motion_optimizer_.inital_base_.lin.a_.setZero();
 
-//  kindr::RotationQuaternionD quat(curr_state.base_.ang.q);
-//  kindr::EulerAnglesZyxD euler(quat);
-//  euler.setUnique(); // to express euler angles close to 0,0,0, not 180,180,180 (although same orientation)
+  motion_optimizer_.SetIntialFootholds(curr_state.GetEEPos());
 
-  motion_optimizer_.inital_base_.ang.p_ = GetEulerZYXAngles(curr_state.base_.ang.q);
-  ROS_INFO_STREAM("Received Current State. Setting only positions, zeroing velocity and accelerations");
+
+
+//  ROS_INFO_STREAM("Received Current State. Setting only positions, zeroing velocity and accelerations");
+//  motion_optimizer_.inital_base_.ang.p_ = GetEulerZYXAngles(curr_state.base_.ang.q);
+
+
+
+  kindr::RotationQuaternionD quat(curr_state.base_.ang.q);
+  kindr::EulerAnglesZyxD euler(quat);
+  euler.setUnique(); // to express euler angles close to 0,0,0, not 180,180,180 (although same orientation)
+//  motion_optimizer_.inital_base_.ang.p_ = euler.toImplementation().reverse();
+
+  // spring_clean_ assuming zero orientation
+  curr_state.base_.ang.q.setIdentity();
+
+//  std::cout << "\n\ncurrent_state: ";
+//  std::cout << curr_state.base_;
+//
+//  std::cout << "all endeffectors:\n";
+//  for (auto ee : curr_state.ee_contact_.GetEEsOrdered()) {
+//    std::cout << curr_state.ee_motion_.At(ee).p_.transpose();
+//    std::cout << curr_state.ee_contact_.At(ee);
+//    std::cout << curr_state.ee_forces_.At(ee).transpose();
+//  }
 }
 
 void
@@ -80,12 +101,7 @@ NlpOptimizerNode::UserCommandCallback(const xpp_msgs::UserCommand& msg)
   user_command_msg_ = msg;
 
   motion_optimizer_.terrain_ = opt::HeightMap::MakeTerrain(static_cast<opt::HeightMap::ID>(msg.terrain_id));
-  // assume flat ground and make sure feet stay at initial height
-  double avg_height=0.0;
-  for ( auto pos : motion_optimizer_.initial_ee_W_.ToImpl())
-    avg_height += pos.z()/motion_optimizer_.initial_ee_W_.GetCount();
-  motion_optimizer_.terrain_->SetGroundHeight(avg_height);
-  ROS_INFO_STREAM("Ground height assumed at " << avg_height);
+  motion_optimizer_.SetTerrainFromAvgFootholdHeight();
 
 
   motion_optimizer_.model_->SetInitialGait(msg.gait_id);
@@ -99,7 +115,16 @@ NlpOptimizerNode::UserCommandCallback(const xpp_msgs::UserCommand& msg)
 
   if (msg.publish_traj) {
     ROS_INFO_STREAM("publishing optimized trajectory to " << cart_trajectory_pub_.getTopic());
-    cart_trajectory_pub_.publish(BuildTrajectoryMsg());
+    auto traj_msg = BuildTrajectoryMsg();
+    cart_trajectory_pub_.publish(traj_msg);
+
+    for (int i=0; i<4; ++i) {
+      std::cout << "xyz feet: ";
+      std::cout << traj_msg.points.front().ee_motion.at(i).pos.x << ", ";
+      std::cout << traj_msg.points.front().ee_motion.at(i).pos.y << ", ";
+      std::cout << traj_msg.points.front().ee_motion.at(i).pos.z << ", ";
+      std::cout << std::endl;
+    }
   }
 
   if (msg.optimize) {
@@ -109,7 +134,7 @@ NlpOptimizerNode::UserCommandCallback(const xpp_msgs::UserCommand& msg)
 
   if (msg.replay_trajectory || msg.optimize) {
     // play back the rosbag hacky like this, as I can't find appropriate C++ API.
-    system(("rosbag play --quiet " + rosbag_name_).c_str());
+    system(("rosbag play --topics " + xpp_msgs::robot_state_desired + " --quiet " + rosbag_name_).c_str());
   }
 }
 
