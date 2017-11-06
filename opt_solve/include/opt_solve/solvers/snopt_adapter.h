b5a00073777a9fe5a1d44c8240c93cf6c54a049d
Alexander Winkler <winklera@ethz.ch>
2017-11-06 20:38:01 +0100

    xpp_solve: add copyright boilerplate

--- opt_solve/include/opt_solve/solvers/snopt_adapter.h
+++ opt_solve/include/opt_solve/solvers/snopt_adapter.h
@@ -1,12 +1,38 @@
+/******************************************************************************
+Copyright (c) 2017, Alexander W. Winkler, ETH Zurich. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+    * Neither the name of ETH ZURICH nor the names of its contributors may be
+      used to endorse or promote products derived from this software without
+      specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL ETH ZURICH BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
+
 /**
- @file    snopt_adapter.h
+@file    snopt_adapter.h
  @author  Alexander W. Winkler (winklera@ethz.ch)
  @date    Jul 4, 2016
  @brief   Declares the SnoptAdapter class
  */
 
-#ifndef XPP_XPP_OPT_INCLUDE_XPP_OPT_SNOPT_ADAPTER_H_
-#define XPP_XPP_OPT_INCLUDE_XPP_OPT_SNOPT_ADAPTER_H_
+#ifndef OPT_SOLVE_INCLUDE_OPT_SNOPT_ADAPTER_H_
+#define OPT_SOLVE_INCLUDE_OPT_SNOPT_ADAPTER_H_
 
 #include <snoptProblem.hpp>
 
@@ -49,4 +75,4 @@ private:
 
 } /* namespace opt */
 
-#endif /* XPP_XPP_OPT_INCLUDE_XPP_OPT_SNOPT_ADAPTER_H_ */
+#endif /* OPT_SOLVE_INCLUDE_OPT_SNOPT_ADAPTER_H_ */
