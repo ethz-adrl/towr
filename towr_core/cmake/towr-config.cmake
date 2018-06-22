d1b4a12705e95ce82855a872afbe318ca0264af0
Alexander Winkler <winklera@ethz.ch>
2018-06-22 16:51:36 +0200

    Added cmake find script to towr_core

---
+++ towr_core/cmake/towr-config.cmake
@@ -0,0 +1,27 @@
+# towr-config.cmake
+# ---------
+#
+# Locates the towr library and includes in cmake project.
+# Copyright (c) 2018, Alexander W. Winkler. All rights reserved.
+#
+#
+# Imported targets
+# ^^^^^^^^^^^^^^^^
+#
+# This module defines the following IMPORTED targets:
+#   towr::towr_core  - variables and constraints for legged locomotion
+#
+#
+# Example usage
+# ^^^^^^^^^^^^^
+#
+#   find_package(towr REQUIRED)
+#   add_executable(foo my_problem.cc)
+#   target_link_libraries(foo PUBLIC towr::towr_core)
+#
+#=============================================================================
+
+
+# these are autogenerate by cmake
+include("${CMAKE_CURRENT_LIST_DIR}/towr_core-targets.cmake")
+
