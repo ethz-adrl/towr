d1b38f64f844b03ea4cb851cf21f0aec1e7dea5c
Alexander Winkler <winklera@ethz.ch>
2018-07-02 15:43:54 +0200

    remove all catkin macros from towr::CMakeLists.txt

--- towr/cmake/towr-config.cmake
+++ towr/cmake/towr-config.cmake
@@ -19,9 +19,24 @@
 #   add_executable(foo my_problem.cc)
 #   target_link_libraries(foo PUBLIC towr::towr)
 #
+#
+# Result variables
+# ^^^^^^^^^^^^^^^^
+#
+# This module will set the following variables in your project. 
+# These can be neccessary when building with catkin and not modern cmake
+#   towr_INCLUDE_DIRS    - path to public include (.h) files
+#   towr_LIBRARIES       - path to all libraries
+#
 #=============================================================================
-
+include(CMakeFindDependencyMacro)
+find_dependency(ifopt)
 
 # these are autogenerate by cmake
 include("${CMAKE_CURRENT_LIST_DIR}/towr-targets.cmake")
 
+get_target_property(towr_INCLUDE_DIRS towr::towr INTERFACE_INCLUDE_DIRECTORIES)
+list(APPEND towr_INCLUDE_DIRS ${ifopt_INCLUDE_DIRS})
+
+get_property(towr_LIBRARIES TARGET towr::towr PROPERTY LOCATION)
+list(APPEND towr_LIBRARIES ${ifopt_LIBRARIES})
